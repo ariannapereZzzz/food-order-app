0855ad6404d688d2fcd505d77f227e8bda6230d4
Arianna Perez <akdev.aring@gmail.com>
2023-12-13 23:04:09 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+      <h1>
+        Another Heading
+      </h1>
+    </div>
+  );
+}
+
+export default App;
